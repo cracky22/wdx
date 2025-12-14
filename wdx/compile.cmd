3737c6a73b07fd7a3f4f5cb8588614c725f11a70
Martin Blieninger <104385850+cracky22@users.noreply.github.com>
2025-12-14 11:29:39 +0100

    updated compiler

--- wdx/compile.cmd
+++ wdx/compile.cmd
@@ -13,3 +13,6 @@ ren .\__pycache__\main_window.cpython-313.pyc main_window.pyc
 ren .\__pycache__\project_manager.cpython-313.pyc project_manager.pyc
 ren .\__pycache__\project_window.cpython-313.pyc project_window.pyc
 ren .\__pycache__\server.cpython-313.pyc server.pyc
+mkdir .\__pycache__\com.crackyOS.wdx
+move .\__pycache__\*.pyc .\__pycache__\com.crackyOS.wdx\
+copy ..\wdx_extension.crx .\__pycache__\com.crackyOS.wdx\
