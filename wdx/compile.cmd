2d3ec58d0a034e494602fbf1384ec97a1d8bdff6
Martin Blieninger <104385850+cracky22@users.noreply.github.com>
2026-01-21 07:50:20 +0100

    updated compiler pyton version

--- wdx/compile.cmd
+++ wdx/compile.cmd
@@ -6,13 +6,13 @@ python -m compileall main_window.py
 python -m compileall project_manager.py
 python -m compileall project_window.py
 python -m compileall server.py
-ren .\__pycache__\dialogs.cpython-313.pyc dialogs.pyc
-ren .\__pycache__\constants.cpython-313.pyc constants.pyc
-ren .\__pycache__\main.cpython-313.pyc main.pyc
-ren .\__pycache__\main_window.cpython-313.pyc main_window.pyc
-ren .\__pycache__\project_manager.cpython-313.pyc project_manager.pyc
-ren .\__pycache__\project_window.cpython-313.pyc project_window.pyc
-ren .\__pycache__\server.cpython-313.pyc server.pyc
+ren .\__pycache__\dialogs.cpython-312.pyc dialogs.pyc
+ren .\__pycache__\constants.cpython-312.pyc constants.pyc
+ren .\__pycache__\main.cpython-312.pyc main.pyc
+ren .\__pycache__\main_window.cpython-312.pyc main_window.pyc
+ren .\__pycache__\project_manager.cpython-312.pyc project_manager.pyc
+ren .\__pycache__\project_window.cpython-312.pyc project_window.pyc
+ren .\__pycache__\server.cpython-312.pyc server.pyc
 mkdir .\__pycache__\com.crackyOS.wdx
 move .\__pycache__\*.pyc .\__pycache__\com.crackyOS.wdx\
 copy ..\wdx_extension\wdx_extension.crx .\__pycache__\com.crackyOS.wdx\
