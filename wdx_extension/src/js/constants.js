deaa848c8a6be48d173226286743662fbdba4060
Martin Blieninger <104385850+cracky22@users.noreply.github.com>
2026-01-29 21:18:27 +0100

    updated build date

--- wdx_extension/src/js/constants.js
+++ wdx_extension/src/js/constants.js
@@ -1,5 +1,5 @@
 export const API_BASE = "http://127.0.0.1:8765";
 export const API_ADD = `${API_BASE}/api/add_source`;
 export const API_STATUS = `${API_BASE}/api/status`;
-export const VERSION = "1.3.5";
+export const VERSION = "1.3.6";
 export const BUILDDATE = "2026-01-29";
