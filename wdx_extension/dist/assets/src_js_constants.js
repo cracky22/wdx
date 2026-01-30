7f22bcff340dab692154e60b89feb57554ce7198
Martin Blieninger <104385850+cracky22@users.noreply.github.com>
2026-01-30 18:22:43 +0100

    v1.3.7 assets

--- wdx_extension/dist/assets/src_js_constants.js
+++ wdx_extension/dist/assets/src_js_constants.js
@@ -1,1 +1,1 @@
-const s="http://127.0.0.1:8765",t=`${s}/api/add_source`,a=`${s}/api/status`,A="1.3.6",o="2026-01-30";export{t as A,o as B,A as V,a};
+const s="http://127.0.0.1:8765",t=`${s}/api/add_source`,a=`${s}/api/status`,A="1.3.7",o="2026-01-30";export{t as A,o as B,A as V,a};
