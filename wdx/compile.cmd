4aafecab6141b5084471335e0141c736866315d9
Martin Blieninger <104385850+cracky22@users.noreply.github.com>
2026-01-30 18:28:57 +0100

    updated compiler (added utils + minimap)

--- wdx/compile.cmd
+++ wdx/compile.cmd
@@ -2,16 +2,20 @@ del __pycache__\*.*
 python -m compileall constants.py
 python -m compileall dialogs.py
 python -m compileall main.py
+python -m compileall minimap.py
 python -m compileall main_window.py
 python -m compileall project_manager.py
 python -m compileall project_window.py
+python -m compileall utils.py
 python -m compileall server.py
 ren .\__pycache__\dialogs.cpython-312.pyc dialogs.pyc
 ren .\__pycache__\constants.cpython-312.pyc constants.pyc
 ren .\__pycache__\main.cpython-312.pyc main.pyc
+ren .\__pycache__\minimap.cpython-312.pyc minimap.pyc
 ren .\__pycache__\main_window.cpython-312.pyc main_window.pyc
 ren .\__pycache__\project_manager.cpython-312.pyc project_manager.pyc
 ren .\__pycache__\project_window.cpython-312.pyc project_window.pyc
+ren .\__pycache__\utils.cpython-312.pyc utils.pyc
 ren .\__pycache__\server.cpython-312.pyc server.pyc
 mkdir .\__pycache__\com.crackyOS.wdx
 move .\__pycache__\*.pyc .\__pycache__\com.crackyOS.wdx\
