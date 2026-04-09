e42a5b02c6430b7d7a100485f8b5e765bd78a444
Martin Blieninger <martinblieninger2208@gmail.com>
2026-04-09 21:16:17 +0200

    updated build date

--- wdx/constants.py
+++ wdx/constants.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 
 VERSION = "1.4.0"
-BUILDDATE = "2026-04-08"
+BUILDDATE = "2026-04-09"
 APP_TITLE = f"wdx v{VERSION} ({BUILDDATE})"
 CODENAME = "com.crackyOS.wdx"
 
