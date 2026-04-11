ade1dc42e57ca308e91b657d1329eeb4ac780969
Martin Blieninger <martinblieninger2208@gmail.com>
2026-04-11 14:18:50 +0200

    updated compiler (__main__)

--- wdx/compile.cmd
+++ wdx/compile.cmd
@@ -10,7 +10,7 @@ python -m compileall utils.py
 python -m compileall server.py
 ren .\__pycache__\dialogs.cpython-314.pyc dialogs.pyc
 ren .\__pycache__\constants.cpython-314.pyc constants.pyc
-ren .\__pycache__\main.cpython-314.pyc main.pyc
+ren .\__pycache__\__main__.cpython-314.pyc __main__.pyc
 ren .\__pycache__\minimap.cpython-314.pyc minimap.pyc
 ren .\__pycache__\main_window.cpython-314.pyc main_window.pyc
 ren .\__pycache__\project_manager.cpython-314.pyc project_manager.pyc
