a165000cd90d10c8da753852eebd95aeae086085
Martin Blieninger <martinblieninger2208@gmail.com>
2026-04-25 12:10:46 +0200

    v1.4.3-release

--- wdx_extension/dist/assets/src_js_constants.js
+++ wdx_extension/dist/assets/src_js_constants.js
@@ -1,1 +1,1 @@
-const s="http://127.0.0.1:8765",t=`${s}/api/add_source`,a=`${s}/api/status`,A="1.4.2",o="2026-04-23";export{t as A,o as B,A as V,a};
+const s="http://127.0.0.1:8765",t=`${s}/api/add_source`,a=`${s}/api/status`,A="1.4.3",o="2026-04-25";export{t as A,o as B,A as V,a};
